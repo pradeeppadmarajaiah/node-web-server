e4ea78b7141e202a3edfbe48c36c8f4ab18daf9b
pradeeppadmarajaiahclear <pradeep.padmarajaiah@gmail.com>
2018-03-23 11:37:36 +0530

    test server

---
+++ server.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs=require('fs');
+
+var app = express();
+
+//hbs view
+app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + "/views/partials");
+
+//start the server
+app.listen(3000, () => console.log('server is up!!!.'));
+
+
+
+app.use((req,res,next)=> {
+  var now=new Date().toString();
+  console.log(`This is the middleware ${req.url} : ${req.method} : ${now}`);
+  fs.appendFile("server.log",`${req.url} : ${req.method} : ${now} \n`,(err) =>{
+    if(err) {
+      console.log("Error");
+    }
+  });
+  next();
+});
+
+
+//static pages
+app.use(express.static(__dirname + "/web"));
+
+// app.use((req,res,next)=> {
+//   res.render('main.hbs');
+// });
+
+app.get('/hello', (req, res) => {
+  res.send('<h1>This is the info page</h1>');
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    year: new Date()
+
+  });
+});
+
+app.get('/', (req, res) => {
+  res.render('index.hbs', {
+    year: new Date().getFullYear(),
+    welcome: "Welcome to Pradeep world"
+  });
+});
